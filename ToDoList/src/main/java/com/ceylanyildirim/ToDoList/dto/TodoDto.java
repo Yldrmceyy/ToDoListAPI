6de41250323310d69ce4ae571462153ccb7277b4
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2025-01-10 14:02:23 +0300

    TodoDto created

---
+++ ToDoList/src/main/java/com/ceylanyildirim/ToDoList/dto/TodoDto.java
@@ -0,0 +1,13 @@
+package com.ceylanyildirim.ToDoList.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TodoDto {
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed;
+}
