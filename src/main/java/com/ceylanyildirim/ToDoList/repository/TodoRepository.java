1d8ce91eeb0f870683379ab6a3da9b9aab0eaee1
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2025-01-10 17:16:17 +0300

    Added existsById,existsByTitle  method to TodoRepository

--- src/main/java/com/ceylanyildirim/ToDoList/repository/TodoRepository.java
+++ src/main/java/com/ceylanyildirim/ToDoList/repository/TodoRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+    boolean existsByTitle(String title); // Aynı başlıkta bir Todo'nun var olup olmadığını kontrol eder.
+    boolean existsById(Long id);         // ID'ye göre kontrol.
 }
