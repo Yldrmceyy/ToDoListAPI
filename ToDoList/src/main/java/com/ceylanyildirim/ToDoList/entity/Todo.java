59b0279044e64eea93c8103a12dc31b442fb80aa
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2025-01-10 13:57:33 +0300

    Todo.java entitiy created

--- ToDoList/src/main/java/com/ceylanyildirim/ToDoList/entity/Todo.java
+++ ToDoList/src/main/java/com/ceylanyildirim/ToDoList/entity/Todo.java
@@ -1,8 +1,20 @@
 package com.ceylanyildirim.ToDoList.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-
+@Getter
+@Setter
+@Table(name = "todos")
 public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String title;
+    private String description;
+    private boolean completed;
+
 }
