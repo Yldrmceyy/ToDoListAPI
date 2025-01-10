2d562b8058fe740f844a26dd40f4f100e04ae637
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2025-01-10 14:14:48 +0300

    Completed getAll, create, update, and delete methods for Todo

--- ToDoList/src/main/java/com/ceylanyildirim/ToDoList/service/ITodoService.java
+++ ToDoList/src/main/java/com/ceylanyildirim/ToDoList/service/ITodoService.java
@@ -1,4 +1,13 @@
 package com.ceylanyildirim.ToDoList.service;
 
-public class ITodoService {
+import com.ceylanyildirim.ToDoList.dto.TodoDto;
+
+import java.util.List;
+
+public interface ITodoService {
+    List<TodoDto> getAllTodos();
+    TodoDto createTodo(TodoDto todoDto);
+    TodoDto updateTodo(Long id, TodoDto todoDto);
+    void deleteTodoById(Long id);
+
 }
