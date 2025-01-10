61e382f806775fac3b638c2146d3fa7495cd1178
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2025-01-10 15:13:56 +0300

    Fetch and map all Todos to DTO format

--- src/main/java/com/ceylanyildirim/ToDoList/service/impl/TodoServisImpl.java
+++ src/main/java/com/ceylanyildirim/ToDoList/service/impl/TodoServisImpl.java
@@ -1,4 +1,46 @@
 package com.ceylanyildirim.ToDoList.service.impl;
 
-public class TodoServisImpl {
+import com.ceylanyildirim.ToDoList.dto.TodoDto;
+import com.ceylanyildirim.ToDoList.repository.TodoRepository;
+import com.ceylanyildirim.ToDoList.service.ITodoService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class TodoServisImpl implements ITodoService {
+
+    private TodoRepository todoRepository;
+    private ModelMapper modelMapper;
+
+    public TodoServisImpl(TodoRepository todoRepository, ModelMapper modelMapper) {
+        this.todoRepository = todoRepository;
+        this.modelMapper = modelMapper;
+    }
+
+
+    @Override
+    public List<TodoDto> getAllTodos() {
+        return todoRepository.findAll()
+                .stream()
+                .map(todo -> modelMapper.map(todo,TodoDto.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public TodoDto createTodo(TodoDto todoDto) {
+        return null;
+    }
+
+    @Override
+    public TodoDto updateTodo(Long id, TodoDto todoDto) {
+        return null;
+    }
+
+    @Override
+    public void deleteTodoById(Long id) {
+
+    }
 }
