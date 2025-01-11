0ebc0fe477fcaacfd18832bebbe78419b8d15d5c
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2025-01-11 12:18:59 +0300

    comment line added

--- src/main/java/com/ceylanyildirim/ToDoList/service/ITodoService.java
+++ src/main/java/com/ceylanyildirim/ToDoList/service/ITodoService.java
@@ -4,11 +4,13 @@ import com.ceylanyildirim.ToDoList.dto.TodoDto;
 
 import java.util.List;
 
+/**
+ * ITodoService, iş mantığını tanımlayan arayüzdür.
+ */
 public interface ITodoService {
-    List<TodoDto> getAllTodos();
-    TodoDto getTodoById(Long id);
-    TodoDto createTodo(TodoDto todoDto);
-    TodoDto updateTodo(Long id, TodoDto todoDto);
-    void deleteTodoById(Long id);
-
+    List<TodoDto> getAllTodos(); // Tüm görevleri getirir.
+    TodoDto getTodoById(Long id); // Belirtilen ID'ye göre bir görevi getirir.
+    TodoDto createTodo(TodoDto todoDto); // Yeni bir görev oluşturur.
+    TodoDto updateTodo(Long id, TodoDto todoDto); // Mevcut bir görevi günceller.
+    void deleteTodoById(Long id); // Belirtilen ID'ye sahip bir görevi siler.
 }
